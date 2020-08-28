9016b2d0320417c851d9ab77846e2946efdccf15
Brent Mitton <brent.mitton+github@gmail.com>
2020-08-28 16:43:16 -0400

    add Google analytics to spy on people

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
+import { Fragment } from "react";
+import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <a href="https://twitter.com/JimGaffigan/status/1299206772364902400">
-        <img alt="hey fuck you karen" src="/fuckyoukaren.png" />
-      </a>
+      <Head>
+        <Fragment>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-176715898-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-176715898-1', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+            }}
+          />
+        </Fragment>
+        <title>hey fuck you karen</title>
+      </Head>
+      <div>
+        <a href="https://twitter.com/JimGaffigan/status/1299206772364902400">
+          <img alt="hey fuck you karen" src="/fuckyoukaren.png" />
+        </a>
+      </div>
     </div>
   );
 }
